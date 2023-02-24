07b42e615a5439410e9ec30fce1f6f5147054526
win9 <krrr883@gmail.com>
2023-02-24 14:13:24 +0900

    Feat: 카카오 OAuth 로직 추가 [#6]

--- backend/src/main/java/com/mohaeng/backend/user/oauth/OAuthAttributes.java
+++ backend/src/main/java/com/mohaeng/backend/user/oauth/OAuthAttributes.java
@@ -23,9 +23,20 @@ public class OAuthAttributes {
             case "google":
                 return ofGoogle(userNameAttributeName, attributes);
             case "kakao":
+                return ofKakao(userNameAttributeName, attributes);
             default:
                 throw new IllegalArgumentException("ERROR!!");
         }
+    }
+
+    private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> kakao_account = (Map<String, Object>) attributes.get("kakao_account");  // 카카오로 받은 데이터에서 계정 정보가 담긴 kakao_account 값을 꺼낸다.
+        Map<String, Object> profile = (Map<String, Object>) kakao_account.get("profile");   // 마찬가지로 profile(nickname, image_url.. 등) 정보가 담긴 값을 꺼낸다.
+
+        return new OAuthAttributes(attributes,
+                userNameAttributeName,
+                (String) profile.get("nickname"),
+                (String) kakao_account.get("email"));
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
