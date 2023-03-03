45b8ca69b64eb678ef5536bbdc8a77f8450bb2f2
Unknown <0219alice@gmail.com>
2023-03-03 22:08:44 +0900

    Feat: 연관 관계 설정 및 Entity 생성 [#5]
    - Course와 Place를 연결해주는 CoursePlace Entity 생성
    - Course와 Place 연관 관계 설정

--- backend/src/main/java/com/mohaeng/backend/course/domain/Course.java
+++ backend/src/main/java/com/mohaeng/backend/course/domain/Course.java
@@ -1,23 +1,36 @@
 package com.mohaeng.backend.course.domain;
 
+import com.mohaeng.backend.common.BaseTimeEntity;
+import com.mohaeng.backend.course.dto.request.CoursePlaceSearchReq;
+import com.mohaeng.backend.member.domain.Member;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jakarta.persistence.FetchType.LAZY;
+
 @Entity
 @Getter
-public class Course extends BaseTimeEntity{
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Course extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "course_id")
     private Long id;
 
-//    @OneToMany
-//    @JoinColumn(name = "course_id")
-//    private List<CoursePlace> coursePlaces = new ArrayList<>();
+    @OneToMany
+    @JoinColumn(name = "course_id")
+    private List<CoursePlace> coursePlaces = new ArrayList<>();
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
     @Column(nullable = false)
     private String title;
@@ -36,4 +49,21 @@ public class Course extends BaseTimeEntity{
     private String originName;
     private String imageUrl;
 
+//    @Builder
+//    public Course(List<CoursePlace> coursePlaces, Member member, String title, String nickname, LocalDateTime startDate, LocalDateTime endDate, LocalDateTime deletedDate,
+//                  String content, Integer likeCount, Boolean isPublished, String imageName, String originName, String imageUrl) {
+//        this.coursePlaces = coursePlaces;
+//        this.member = member;
+//        this.title = title;
+//        this.nickname = nickname;
+//        this.startDate = startDate;
+//        this.endDate = endDate;
+//        this.deletedDate = deletedDate;
+//        this.content = content;
+//        this.likeCount = likeCount;
+//        this.isPublished = isPublished;
+//        this.imageName = imageName;
+//        this.originName = originName;
+//        this.imageUrl = imageUrl;
+//    }
 }
