09585580cf827dbdfdfe0e755a907cfc7ca0466d
win9 <krrr883@gmail.com>
2023-03-03 16:33:39 +0900

    Fix: Kakao로그인시 정보를 받아오지 못하는 오류 수정  [#6]

--- backend/src/main/java/com/mohaeng/backend/user/controller/LoginController.java
+++ backend/src/main/java/com/mohaeng/backend/user/controller/LoginController.java
@@ -1,12 +1,14 @@
 package com.mohaeng.backend.user.controller;
 
 import com.mohaeng.backend.user.domain.User;
+import com.mohaeng.backend.common.BaseResponse;
 import com.mohaeng.backend.user.dto.UserLoginDto;
 import com.mohaeng.backend.user.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
@@ -14,7 +16,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
@@ -26,9 +27,9 @@ public class LoginController {
     private final UserRepository userRepository;
 
     @GetMapping("/loginInfo")
-    public Map<String, Object> loginInfoController(Authentication authentication) {
-        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+    public ResponseEntity loginInfoController(@AuthenticationPrincipal OAuth2User oAuth2User) {
         Map<String, Object> attributes = oAuth2User.getAttributes();
+
         Optional<User> optionalUser = userRepository.findByEmail((String) attributes.get("email"));
 
         if (optionalUser.isEmpty()) {
@@ -38,22 +39,14 @@ public class LoginController {
         User findUser = optionalUser.get();
         UserLoginDto userLoginDto = new UserLoginDto(findUser.getId(), findUser.getNickName(), findUser.getEmail());
 
-        HashMap<String, Object> result = new HashMap<>();
-        result.put("status", 200);
-        result.put("result", "ok");
-        result.put("data", userLoginDto);
-
-        return result;
+        return ResponseEntity.ok().body(BaseResponse.success("ok", userLoginDto));
     }
 
     @GetMapping("/user/logout")
-    public HashMap<String, Object> logoutController(HttpServletRequest request, HttpServletResponse response) {
+    public ResponseEntity logoutController(HttpServletRequest request, HttpServletResponse response) {
         new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
 
-        HashMap<String, Object> result = new HashMap<>();
-        result.put("status", 200);
-        result.put("result", "ok");
-        return result;
+        return ResponseEntity.ok().body(BaseResponse.success("ok"));
     }
 
 }
--- backend/src/main/java/com/mohaeng/backend/user/oauth/OAuthAttributes.java
+++ backend/src/main/java/com/mohaeng/backend/user/oauth/OAuthAttributes.java
@@ -30,13 +30,13 @@ public class OAuthAttributes {
     }
 
     private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
-        Map<String, Object> kakao_account = (Map<String, Object>) attributes.get("kakao_account");  // 카카오로 받은 데이터에서 계정 정보가 담긴 kakao_account 값을 꺼낸다.
-        Map<String, Object> profile = (Map<String, Object>) kakao_account.get("profile");   // 마찬가지로 profile(nickname, image_url.. 등) 정보가 담긴 값을 꺼낸다.
-
-        return new OAuthAttributes(attributes,
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");  // 카카오로 받은 데이터에서 계정 정보가 담긴 kakao_account 값을 꺼낸다.
+        Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");   // 마찬가지로 profile(nickname, image_url.. 등) 정보가 담긴 값을 꺼낸다.
+        kakaoAccount.put("id", attributes.get("id"));
+        return new OAuthAttributes(kakaoAccount,
                 userNameAttributeName,
                 (String) profile.get("nickname"),
-                (String) kakao_account.get("email"));
+                (String) kakaoAccount.get("email"));
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
