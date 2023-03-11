b6a2891e4f2931f561c19990b90d7570701fed19
JEONGMIN <gyflsakfn@naver.com>
2023-03-11 16:02:58 +0900

    Feat: 전역 폰트 임시 추가

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -8,9 +8,18 @@ import GlobalModal from "../components/Modal/GlobalModal";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
+import { Noto_Sans_KR } from "next/font/google";
+
+const NotoSansKR = Noto_Sans_KR({ weight: "400", subsets: ["latin"] });
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <style jsx global>{`
+        html {
+          font-family: ${NotoSansKR.style.fontFamily};
+        }
+      `}</style>
       <Head>
         <title>모두의 여행</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
