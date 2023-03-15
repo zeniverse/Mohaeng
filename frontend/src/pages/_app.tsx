6e512a0d2432edb40359f90e1f766b91bbfaa69e
JEONGMIN <gyflsakfn@naver.com>
2023-03-15 16:24:00 +0900

    Design: fetch&Bannser Search css 수정

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -9,14 +9,6 @@ import AppLayout from "../components/Layout/AppLayout";
 import { Noto_Sans_KR } from "next/font/google";
 
 const NotoSansKR = Noto_Sans_KR({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-  display: "swap",
-});
-
-import { Noto_Sans_KR } from "next/font/google";
-
-const NotoSansKR = Noto_Sans_KR({
   weight: "400",
   subsets: ["latin"],
   display: "swap",
