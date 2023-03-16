d8c0b7dfae4f8fa36c66511590e3bce1befdb09d
win9 <krrr883@gmail.com>
2023-03-16 23:32:30 +0900

    fix: 세션 지우는 로직 추가 설정

--- backend/src/main/java/com/mohaeng/backend/member/controller/LoginController.java
+++ backend/src/main/java/com/mohaeng/backend/member/controller/LoginController.java
@@ -6,6 +6,7 @@ import com.mohaeng.backend.member.dto.response.MemberLoginDto;
 import com.mohaeng.backend.member.repository.MemberRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -13,6 +14,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -22,6 +24,7 @@ import java.util.Optional;
 @Controller
 @ResponseBody
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LoginController {
 
     private final MemberRepository memberRepository;
@@ -45,6 +48,8 @@ public class LoginController {
     @GetMapping("/user/logout")
     public ResponseEntity logoutController(HttpServletRequest request, HttpServletResponse response) {
         new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        HttpSession session = request.getSession();
+        session.invalidate();
 
         return ResponseEntity.ok().body(BaseResponse.success("ok"));
     }
