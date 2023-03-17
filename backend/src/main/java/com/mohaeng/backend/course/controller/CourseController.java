a3e7e37d400048f082202ffda560ce6704c6ea94
zeniverse <0219alice@gmail.com>
2023-03-17 21:12:43 +0900

    Feat : Course 생성 기능 구현 - Controller [#27]
    Course 생성 기능을 구현함.
    CourseIdRes는 생성한 course의 id값을 return 해주기 위해 만든 Response DTO

--- backend/src/main/java/com/mohaeng/backend/course/controller/CourseController.java
+++ backend/src/main/java/com/mohaeng/backend/course/controller/CourseController.java
@@ -2,16 +2,20 @@ package com.mohaeng.backend.course.controller;
 
 import com.mohaeng.backend.common.BaseResponse;
 import com.mohaeng.backend.course.dto.request.CoursePlaceSearchReq;
+import com.mohaeng.backend.course.dto.request.CourseReq;
+import com.mohaeng.backend.course.dto.response.CourseIdRes;
 import com.mohaeng.backend.course.dto.response.CoursePlaceSearchRes;
 import com.mohaeng.backend.course.service.CourseService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/course")
@@ -26,5 +30,19 @@ public class CourseController {
 
         CoursePlaceSearchRes res = courseService.placeSearch(req, pageable);
         return ResponseEntity.ok().body(BaseResponse.success("OK", res));
+    }
+
+    @PostMapping
+    public ResponseEntity createCourse(@AuthenticationPrincipal OAuth2User oAuth2User,
+                                       @Valid @RequestBody CourseReq courseReq){
+
+        //TODO: @Valid 결과를 RestcontrollerAdvice를 통해 처리하도록 수정해야함
+
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        String memberEmail = (String) attributes.get("email");
+
+        CourseIdRes courseIdRes = courseService.createCourse(courseReq, memberEmail);
+
+        return ResponseEntity.ok().body(BaseResponse.success("OK", courseIdRes));
     }
 }
