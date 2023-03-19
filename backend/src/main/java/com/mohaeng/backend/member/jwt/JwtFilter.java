3265fba55bde2883f8921fd4782d4cfe96641546
win9 <krrr883@gmail.com>
2023-03-19 23:20:37 +0900

    Feat: 토큰검증을 위한 JwtFilter 추가 [#58]

---
+++ backend/src/main/java/com/mohaeng/backend/member/jwt/JwtFilter.java
@@ -0,0 +1,47 @@
+package com.mohaeng.backend.member.jwt;
+
+import com.mohaeng.backend.member.domain.Member;
+import com.mohaeng.backend.member.domain.Role;
+import com.mohaeng.backend.member.repository.MemberRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+@RequiredArgsConstructor
+public class JwtFilter extends GenericFilterBean {
+    private final TokenGenerator tokenGenerator;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String accessToken = httpServletRequest.getHeader("Access-Token");
+
+        if (accessToken != null && tokenGenerator.checkToken(accessToken)) {
+            String email = tokenGenerator.parseEmailFromToken(accessToken);
+            Member member = memberRepository.findByEmail(email).get();
+
+            Authentication authentication = getAuthentication(member);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    public Authentication getAuthentication(Member member) {
+        return new UsernamePasswordAuthenticationToken(member, "",
+                Arrays.asList(new SimpleGrantedAuthority(Role.NORMAL.name())));
+    }
+}
