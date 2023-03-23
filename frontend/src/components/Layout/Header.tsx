e5e9a49ee5504dcdae5070bea8e40a7c2e0f91b8
saerom <sagesaeromkim@gmail.com>
2023-03-23 17:31:29 +0900

    Feat: token reducer생성, 로그인 로직 추가#58

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -6,9 +6,17 @@ import styles from "./Header.module.css";
 import { BsSearch } from "react-icons/bs";
 import { FaUserCircle } from "react-icons/fa";
 import styled from "styled-components";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { openModal } from "../../store/reducers/modalSlice";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import {
+  setEmail,
+  setId,
+  setNickname,
+  setToken,
+} from "@/src/store/reducers/loginTokenSlice";
+import { RootState } from "@/src/store/store";
 
 const StyledIcon = styled(BsSearch)`
   color: #004aad;
@@ -17,7 +25,10 @@ const StyledIcon = styled(BsSearch)`
 type Props = {};
 
 function Header({}: Props) {
+  const router = useRouter();
   const dispatch = useDispatch();
+  const loginToken = useSelector((state: RootState) => state.token.token);
+  const nickname = useSelector((state: RootState) => state.nickname.nickname);
 
   const handleOpenLoginModal = () => {
     dispatch(
@@ -34,6 +45,16 @@ function Header({}: Props) {
         isOpen: true,
       })
     );
+  };
+
+  const handleLogout = () => {
+    localStorage.clear(); // 토큰 삭제, removeItem
+    dispatch(setToken(""));
+    dispatch(setNickname(""));
+    dispatch(setEmail(""));
+    dispatch(setId(0));
+    router.replace("/");
+    window.alert("로그아웃되었습니다!");
   };
 
   return (
@@ -54,56 +75,47 @@ function Header({}: Props) {
             </button>
           </div>
           <div className={styles.menu}>
-            <Link href="#">여행지</Link>
-            <Link href="course">코스</Link>
-            {/* <Link href="#">동행 게시판</Link> */}
+            <Link href="/place">여행지</Link>
+            <Link href="/course">코스</Link>
             <Link href="/mypage">마이페이지</Link>
           </div>
         </div>
       </nav>
       <div className={styles.btn}>
-        <button
-          id="login-btn"
-          className={styles["login-btn"]}
-          onClick={handleOpenLoginModal}
-          // onClick={() => signIn("kakao")}
-        >
-          로그인
-        </button>
-        {/* {!session ? (
+        {!loginToken ? (
           <>
-            
-          </>
-        ) : (
-          <>
-            <Image
-              className={styles["kakao-profile-img"]}
-              src={session.user?.image}
-              alt="카카오프로필"
-              width={40}
-              height={40}
-            />
-            {session.user?.name}님
             <button
               id="login-btn"
               className={styles["login-btn"]}
-              // onClick={handleOpenLoginModal}
-              onClick={() => signOut()}
+              onClick={handleOpenLoginModal}
+            >
+              로그인
+            </button>
+          </>
+        ) : (
+          <>
+            {nickname}님
+            <button
+              id="login-btn"
+              className={styles["login-btn"]}
+              onClick={handleLogout}
             >
               로그아웃
             </button>
           </>
-        )} */}
-        <button
-          id="signup-btn"
-          className={styles["signup-btn"]}
-          onClick={handleOpenBasicModal}
-        >
-          기본 모달
-        </button>
+        )}
       </div>
     </header>
   );
+}
+
+{
+  /* <Image
+              className={styles["kakao-profile-img"]}
+              src={session.user?.image}
+              alt="카카오프로필"
+              width={40}
+              height={40} /> */
 }
 
 export default Header;
