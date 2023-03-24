c91b2440200184d92c1f894fdfafb19ca54d3664
win9 <krrr883@gmail.com>
2023-03-25 01:19:07 +0900

    Feat: 토큰만료시의 Refersh로직 추가 [#58]

--- backend/src/main/java/com/mohaeng/backend/member/controller/LoginController.java
+++ backend/src/main/java/com/mohaeng/backend/member/controller/LoginController.java
@@ -4,69 +4,73 @@ import com.mohaeng.backend.member.domain.Member;
 import com.mohaeng.backend.common.BaseResponse;
 import com.mohaeng.backend.member.dto.response.MemberLoginDto;
 import com.mohaeng.backend.member.jwt.Token;
+import com.mohaeng.backend.member.jwt.TokenGenerator;
 import com.mohaeng.backend.member.service.MemberService;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.io.IOException;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
 @ResponseBody
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LoginController {
 
     private final MemberService memberService;
+    private final TokenGenerator tokenGenerator;
 
-//    @GetMapping("/loginInfo")
-//    public ResponseEntity loginInfoController(@AuthenticationPrincipal OAuth2User oAuth2User) {
-//        Map<String, Object> attributes = oAuth2User.getAttributes();
-//        System.out.println("LoginController.loginInfoController");
-//        Member findMember = memberService.findByEmail((String) attributes.get("email"));
-//
-//        System.out.println("findMember.getName() = " + findMember.getName());
-//        MemberLoginDto memberLoginDto = new MemberLoginDto(findMember.getId(), findMember.getNickName(), findMember.getEmail());
-//
-//        return ResponseEntity.ok().body(BaseResponse.success("ok", memberLoginDto));
-//    }
-
-    @GetMapping("/user/logout")
-    public ResponseEntity logoutController(HttpServletRequest request, HttpServletResponse response) {
-        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
-        HttpSession session = request.getSession();
-        session.invalidate();
-
-        return ResponseEntity.ok().body(BaseResponse.success("ok"));
-    }
 
     @GetMapping("/oauth/token")
-    public Token getLogin(@RequestParam("code") String code) throws IOException {
+    public ResponseEntity getToken(@RequestParam("code") String code, HttpServletResponse response) {
         String accessToken = memberService.getAccessToken(code);
 
         Member member = memberService.saveMember(accessToken);
+        member.setOauthAccessToken(accessToken);
         Token token = memberService.createToken(member);
-        return token;
+
+        Cookie cookie = new Cookie("Access-Token", token.getAccessToken());
+        cookie.setAttribute("Response-Token", token.getRefreshToken());
+        cookie.setMaxAge(60*60*24);
+        response.addCookie(cookie);
+
+        return ResponseEntity.ok().body(BaseResponse.success("ok", ""));
+    }
+
+    /**
+     * 토큰 재발급
+     *
+     * @param request
+     * @param response
+     * @return
+     */
+    @GetMapping("/oauth/token/refresh")
+    public Token refreshAuth(HttpServletRequest request, HttpServletResponse response) {
+        String token = request.getHeader("Refresh-token");
+
+        if (token != null && tokenGenerator.checkToken(token)) {
+            String email = tokenGenerator.parseEmailFromToken(token);
+            Member member = memberService.findByEmail(email);
+            Token reGeneratedToken = memberService.createToken(member);
+
+            return reGeneratedToken;
+        }
+
+        throw new IllegalArgumentException("NotExistRefreshToken");
     }
 
     @GetMapping("/loginInfo")
-    public ResponseEntity<Object> loginInfoController(HttpServletRequest request) { //(1)
-
+    public ResponseEntity loginInfoController(HttpServletRequest request) {
         String userEmail = (String) request.getAttribute("userEmail");
+
         Member findMember = memberService.findByEmail(userEmail);
-        MemberLoginDto memberLoginDto = new MemberLoginDto(findMember.getId(), findMember.getNickName(), findMember.getEmail());
+        MemberLoginDto memberLoginDto = new MemberLoginDto(findMember.getId(), findMember.getNickName(), findMember.getEmail(), findMember.getImageURL());
 
         return ResponseEntity.ok().body(BaseResponse.success("ok", memberLoginDto));
     }
+
 
 }
