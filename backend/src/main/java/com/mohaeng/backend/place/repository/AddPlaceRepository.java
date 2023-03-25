7e60c119507e43276336e152be979f7d4f2e3685
binaryhong <binaryhong@gmail.com>
2023-03-26 01:37:06 +0900

    Remove: 쓰지않는 AddPlaceRepository 주석 처리

--- backend/src/main/java/com/mohaeng/backend/place/repository/AddPlaceRepository.java
+++ backend/src/main/java/com/mohaeng/backend/place/repository/AddPlaceRepository.java
@@ -1,9 +1,7 @@
-package com.mohaeng.backend.place.repository;
-
-import com.mohaeng.backend.place.domain.AddPlace;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddPlaceRepository extends JpaRepository<AddPlace, Long> {
-}
+//package com.mohaeng.backend.place.repository;
+//
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface AddPlaceRepository extends JpaRepository<AddPlace, Long> {
+//}
