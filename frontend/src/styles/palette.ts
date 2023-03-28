486f20fb803830d78da745249c99599d9e13fc98
JEONGMIN <gyflsakfn@naver.com>
2023-03-28 14:56:46 +0900

    Refactor: Tag의 재사용성 향상

--- frontend/src/styles/palette.ts
+++ frontend/src/styles/palette.ts
@@ -1,8 +1,12 @@
+// D: 진한 M: 중간 L: 연한
 export const palette = {
-  green: "#DFFFD8",
-  sky: "#B4E4FF",
-  beige: "#FEFBE9",
-  grey: "#f3f3f3",
+  Dgreen: "#7AA874",
+  Lsky: "#B4E4FF",
+  Msky: "#62CDFF",
+  Lbeige: "#FEFBE9",
+  Lgrey: "#f3f3f3",
+  Mpink: "#F7C8E0",
+  Dyellow: "#F2CD5C",
 };
 
 export type PaletteKeyTypes = keyof typeof palette;
