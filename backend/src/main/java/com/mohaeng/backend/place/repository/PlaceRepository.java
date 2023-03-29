1b086207c5b321e581fdd2b18319207dd3c658e5
binaryhong <binaryhong@gmail.com>
2023-03-30 03:09:07 +0900

    Feat: findByAreaCodeEquals 추가

--- backend/src/main/java/com/mohaeng/backend/place/repository/PlaceRepository.java
+++ backend/src/main/java/com/mohaeng/backend/place/repository/PlaceRepository.java
@@ -1,6 +1,9 @@
 package com.mohaeng.backend.place.repository;
 
 import com.mohaeng.backend.place.domain.Place;
+import com.mohaeng.backend.place.dto.FindAllPlacesDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +14,5 @@ public interface PlaceRepository extends JpaRepository<Place, Long>, PlaceReposi
     List<Place> findByAddressContainingIgnoreCase(String searchValue);
     List<Place> findByNameContainingOrAddressContaining(String name, String address);
     List<Place> findByNameContaining(String name);
-
+    Page<FindAllPlacesDto> findByAreaCodeEquals(String areaCode, Pageable pageable);
 }
