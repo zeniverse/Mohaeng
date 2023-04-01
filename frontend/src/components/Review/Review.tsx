608d91eaf2f750a37cd8baf1a8fbd6d5f91a319d
saerom <sagesaeromkim@gmail.com>
2023-04-01 17:52:46 +0900

    Feat: 리뷰 정렬 기능 추가 [#17]

--- frontend/src/components/Review/Review.tsx
+++ frontend/src/components/Review/Review.tsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
+import { useState } from "react";
 import styles from "./Review.module.css";
 import ReviewBox from "./ReviewBox";
 
 // 해당 여행지 총 리뷰 건수, 별점 데이터 가져오기
 // 정렬 필터
 export default function Review() {
+  const [selectedValue, setSelectedValue] = useState("default");
+
+  const handleChange = (e) => {
+    setSelectedValue(e.target.value);
+  };
+
   return (
     <>
       <section className={styles.review}>
@@ -27,8 +34,12 @@ export default function Review() {
               <p>총 리뷰 건수</p>
               <span>평균 별점</span>
             </div>
-            <select className={styles.select} name="sort">
-              <option value="" selected>
+            <select
+              className={styles.select}
+              value={selectedValue}
+              onChange={handleChange}
+            >
+              <option value="default" selected>
                 정렬 ▼
               </option>
               <option value="newest">최신순</option>
