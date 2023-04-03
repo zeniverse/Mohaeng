58732b203c70bb87efeb099447af446d8b447e05
binaryhong <binaryhong@gmail.com>
2023-04-03 19:34:18 +0900

    Feat: PlaceBookmark 추가

---
+++ backend/src/main/java/com/mohaeng/backend/place/repository/PlaceBookmarkRepository.java
@@ -0,0 +1,14 @@
+package com.mohaeng.backend.place.repository;
+
+import com.mohaeng.backend.member.domain.Member;
+import com.mohaeng.backend.place.domain.Place;
+import com.mohaeng.backend.place.domain.PlaceBookmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlaceBookmarkRepository extends JpaRepository<PlaceBookmark, Long> {
+    boolean existsPlaceBookmarkByMemberAndPlace(Member member, Place place);
+    boolean existsPlaceBookmarkByMemberAndId(Member member, Long id);
+    PlaceBookmark findByMemberAndPlace(Member member, Place place);
+}
