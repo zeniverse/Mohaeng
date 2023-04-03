1ca2a165ac1151845128b4a35146b14c6d3414d5
binaryhong <binaryhong@gmail.com>
2023-04-03 18:27:27 +0900

    Feat: PlaceBookmark 추가

--- backend/src/main/java/com/mohaeng/backend/place/domain/PlaceBookmark.java
+++ backend/src/main/java/com/mohaeng/backend/place/domain/PlaceBookmark.java
@@ -1,21 +1,24 @@
 package com.mohaeng.backend.place.domain;
 
+import com.mohaeng.backend.common.BaseTimeEntity;
 import com.mohaeng.backend.member.domain.Member;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.Where;
+
+import java.time.LocalDateTime;
 
 import static jakarta.persistence.FetchType.LAZY;
 
-@Table
-@Builder
-@Getter
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Bookmark {
+@Where(clause = "deleted_date is NULL")
+public class PlaceBookmark extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "bookmark_id")
+    @Column(name = "place_bookmark_id")
     private Long id;
 
     @ManyToOne(fetch = LAZY)
@@ -26,12 +29,24 @@ public class Bookmark {
     @JoinColumn(name = "place_id")
     private Place place;
 
+    private LocalDateTime deletedDate;
 
-
-    public Bookmark(Long id, Member member, Place place) {
+    @Builder
+    public PlaceBookmark(Long id, Place place, Member member, LocalDateTime createdDate, LocalDateTime deletedDate) {
         this.id = id;
-        this.member = member;
         this.place = place;
+        this.member = member;
+        this.deletedDate = deletedDate;
     }
 
+    public static PlaceBookmark of(Member member, Place place){
+        return PlaceBookmark.builder()
+                .member(member)
+                .place(place)
+                .build();
+    }
+
+    public void updateDeletedDate() {
+        this.deletedDate = LocalDateTime.now();
+    }
 }
