453918b69aaf4ae38fef0361e455b10175937093
binaryhong <binaryhong@gmail.com>
2023-04-03 19:33:38 +0900

    fix: remot merged conflict

--- backend/src/test/java/com/mohaeng/backend/course/controller/CourseLikesControllerTest.java
+++ backend/src/test/java/com/mohaeng/backend/course/controller/CourseLikesControllerTest.java
@@ -28,124 +28,121 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
                 @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
         })
 class CourseLikesControllerTest {
-//    @Autowired
-//    private MockMvc mockMvc;
-//    @MockBean private CourseLikesService courseLikesService;
-//
-//
-//    @Test
-//    @DisplayName("[POST] 코스 좋아요 - 정상 처리")
-//    @WithMockUser
-//    public void addCourseLikes() throws Exception {
-//        //Given
-//        given(courseLikesService.addLikes(anyLong(), anyString()))
-//                .willReturn(CourseLikesRes.from(1L, 1L));
-//        Long courseId = 100L;
-//
-//        //When & Then
-//        mockMvc.perform(post("/api/course/likes/{courseId}", courseId)
-//                        .with(oauth2Login()
-//                                // 1
-//                                .authorities(new SimpleGrantedAuthority("ROLE_NORMAL"))
-//                                // 2
-//                                .attributes(attributes -> {
-//                                    attributes.put("name", "kimMohaeng");
-//                                    attributes.put("email", "test@test.com");
-//                                })
-//                        )
-//                        .with(csrf()))
-//                .andExpect(status().isOk())
-//                .andDo(print());
-//
-//        verify(courseLikesService).addLikes(eq(courseId), eq("test@test.com"));
-//    }
-//
-//    @Test
-//    @DisplayName("[DELETE] 코스 좋아요 취소 - 정상 처리")
-//    @WithMockUser
-//    public void cancelCourseLikes() throws Exception {
-//        //Given
-//        given(courseLikesService.cancelLikes(anyLong(), anyString()))
-//                .willReturn(CourseLikesRes.from(1L, 1L));
-//        Long courseId = 100L;
-//
-//        //When & Then
-//        mockMvc.perform(delete("/api/course/likes/{courseId}", courseId)
-//                        .with(oauth2Login()
-//                                // 1
-//                                .authorities(new SimpleGrantedAuthority("ROLE_NORMAL"))
-//                                // 2
-//                                .attributes(attributes -> {
-//                                    attributes.put("name", "kimMohaeng");
-//                                    attributes.put("email", "test@test.com");
-//                                })
-//                        )
-//                        .with(csrf()))
-//                .andExpect(status().isOk())
-//                .andDo(print());
-//
-//        verify(courseLikesService).cancelLikes(eq(courseId), eq("test@test.com"));
-//    }
-//
-//    @Test
-//    @DisplayName("[GET] 코스 좋아요 유무 - 정상 처리")
-//    @WithMockUser
-//    public void existsCourseLikes() throws Exception {
-//        //Given
-//        Long courseId = 1L;
-//        given(courseLikesService.isExistCourseLikes(anyLong(), anyString())).willReturn(false);
-//
-//        //When & Then
-//        mockMvc.perform(get("/api/course/likes/{courseId}", courseId)
-//                        .with(oauth2Login()
-//                                // 1
-//                                .authorities(new SimpleGrantedAuthority("ROLE_NORMAL"))
-//                                // 2
-//                                .attributes(attributes -> {
-//                                    attributes.put("name", "kimMohaeng");
-//                                    attributes.put("email", "test@test.com");
-//                                })
-//                        ))
-//                .andExpect(status().isOk())
-//                .andDo(print());
-//
-//        verify(courseLikesService).isExistCourseLikes(eq(courseId), eq("test@test.com"));
-//    }
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean private CourseLikesService courseLikesService;
 
-//    @Test
-//    @DisplayName("[GET] 코스 좋아요 count - 정상 처리")
-//    @WithMockUser
-//    public void countCourseLikes() throws Exception {
-//        //Given
-//        Long courseId = 1L;
-//        given(courseLikesService.countLikes(anyLong()))
-//                .willReturn(CourseLikesRes.from(1L, 1L));
-//
-//        //When & Then
-//        mockMvc.perform(get("/api/course/likes/count/{courseId}", courseId))
-//                .andExpect(status().isOk())
-//                .andDo(print());
-//
-//        verify(courseLikesService).countLikes(eq(courseId));
-//    }
-//
-//
-//
-//    //TODO: exceptionHandler 구현 후, 처리할 case
-//
-//    // 코스 좋아요 - 로그인한 유저가 아닐 경우
-//    // 코스 좋아요 - 코스가 존재하지 않는 경우
-//    // 코스 좋아요 - 이미 좋아요를 누른 경우
-//
-//    // 코스 좋아요 취소 - 로그인한 유저가 아닐 경우
-//    // 코스 좋아요 취소 - 코스가 존재하지 않는 경우
-//    // 코스 좋아요 취소 - 좋아요를 누르지 않은 경우
-//
-//    // 코스 좋아요 유무 - 로그인 되어있지 않을 때
-//
-//    // 코스 좋아요 갯수 조회 - 코스가 존재하지 않는 경우
-<<<<<<< HEAD
-//}
-=======
+
+    @Test
+    @DisplayName("[POST] 코스 좋아요 - 정상 처리")
+    @WithMockUser
+    public void addCourseLikes() throws Exception {
+        //Given
+        given(courseLikesService.addLikes(anyLong(), anyString()))
+                .willReturn(CourseLikesRes.from(1L, 1L));
+        Long courseId = 100L;
+
+        //When & Then
+        mockMvc.perform(post("/api/course/likes/{courseId}", courseId)
+                        .with(oauth2Login()
+                                // 1
+                                .authorities(new SimpleGrantedAuthority("ROLE_NORMAL"))
+                                // 2
+                                .attributes(attributes -> {
+                                    attributes.put("name", "kimMohaeng");
+                                    attributes.put("email", "test@test.com");
+                                })
+                        )
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        verify(courseLikesService).addLikes(eq(courseId), eq("test@test.com"));
+    }
+
+    @Test
+    @DisplayName("[DELETE] 코스 좋아요 취소 - 정상 처리")
+    @WithMockUser
+    public void cancelCourseLikes() throws Exception {
+        //Given
+        given(courseLikesService.cancelLikes(anyLong(), anyString()))
+                .willReturn(CourseLikesRes.from(1L, 1L));
+        Long courseId = 100L;
+
+        //When & Then
+        mockMvc.perform(delete("/api/course/likes/{courseId}", courseId)
+                        .with(oauth2Login()
+                                // 1
+                                .authorities(new SimpleGrantedAuthority("ROLE_NORMAL"))
+                                // 2
+                                .attributes(attributes -> {
+                                    attributes.put("name", "kimMohaeng");
+                                    attributes.put("email", "test@test.com");
+                                })
+                        )
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        verify(courseLikesService).cancelLikes(eq(courseId), eq("test@test.com"));
+    }
+
+    @Test
+    @DisplayName("[GET] 코스 좋아요 유무 - 정상 처리")
+    @WithMockUser
+    public void existsCourseLikes() throws Exception {
+        //Given
+        Long courseId = 1L;
+        given(courseLikesService.isExistCourseLikes(anyLong(), anyString())).willReturn(false);
+
+        //When & Then
+        mockMvc.perform(get("/api/course/likes/{courseId}", courseId)
+                        .with(oauth2Login()
+                                // 1
+                                .authorities(new SimpleGrantedAuthority("ROLE_NORMAL"))
+                                // 2
+                                .attributes(attributes -> {
+                                    attributes.put("name", "kimMohaeng");
+                                    attributes.put("email", "test@test.com");
+                                })
+                        ))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        verify(courseLikesService).isExistCourseLikes(eq(courseId), eq("test@test.com"));
+    }
+
+    @Test
+    @DisplayName("[GET] 코스 좋아요 count - 정상 처리")
+    @WithMockUser
+    public void countCourseLikes() throws Exception {
+        //Given
+        Long courseId = 1L;
+        given(courseLikesService.countLikes(anyLong()))
+                .willReturn(CourseLikesRes.from(1L, 1L));
+
+        //When & Then
+        mockMvc.perform(get("/api/course/likes/count/{courseId}", courseId))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        verify(courseLikesService).countLikes(eq(courseId));
+    }
+
+
+
+    //TODO: exceptionHandler 구현 후, 처리할 case
+
+    // 코스 좋아요 - 로그인한 유저가 아닐 경우
+    // 코스 좋아요 - 코스가 존재하지 않는 경우
+    // 코스 좋아요 - 이미 좋아요를 누른 경우
+
+    // 코스 좋아요 취소 - 로그인한 유저가 아닐 경우
+    // 코스 좋아요 취소 - 코스가 존재하지 않는 경우
+    // 코스 좋아요 취소 - 좋아요를 누르지 않은 경우
+
+    // 코스 좋아요 유무 - 로그인 되어있지 않을 때
+
+    // 코스 좋아요 갯수 조회 - 코스가 존재하지 않는 경우
+
 }
->>>>>>> 1303caaaefa9ca7751c9e663fe23ef79e5e04d00
