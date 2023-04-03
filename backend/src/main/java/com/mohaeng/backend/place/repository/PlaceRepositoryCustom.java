7349b56d9b16954a1a8e087288e42eb1786bb471
binaryhong <binaryhong@gmail.com>
2023-04-03 19:14:53 +0900

    Refactor: import  삭제

--- backend/src/main/java/com/mohaeng/backend/place/repository/PlaceRepositoryCustom.java
+++ backend/src/main/java/com/mohaeng/backend/place/repository/PlaceRepositoryCustom.java
@@ -1,7 +1,5 @@
 package com.mohaeng.backend.place.repository;
 
-import com.mohaeng.backend.course.dto.CoursePlaceSearchDto;
-import com.mohaeng.backend.course.dto.response.CoursePlaceSearchRes;
 import com.mohaeng.backend.place.domain.Place;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
