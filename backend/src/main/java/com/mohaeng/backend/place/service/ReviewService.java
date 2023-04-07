b65b4933f49c2c1ccc4cc643b4bba11dbd6900ba
binaryhong <binaryhong@gmail.com>
2023-04-08 03:09:46 +0900

    Feat: 수정, 삭제, CreatedDate 추가, JPA CACHE에러 해결

--- backend/src/main/java/com/mohaeng/backend/place/service/ReviewService.java
+++ backend/src/main/java/com/mohaeng/backend/place/service/ReviewService.java
@@ -1,21 +1,28 @@
 package com.mohaeng.backend.place.service;
 
+import com.mohaeng.backend.Image.AmazonS3Service;
 import com.mohaeng.backend.exception.notfound.MemberNotFoundException;
+import com.mohaeng.backend.exception.notfound.ReviewNotFoundException;
 import com.mohaeng.backend.member.domain.Member;
 import com.mohaeng.backend.member.repository.MemberRepository;
 import com.mohaeng.backend.place.domain.Place;
 import com.mohaeng.backend.place.domain.Review;
 import com.mohaeng.backend.place.domain.ReviewImage;
 import com.mohaeng.backend.place.dto.request.CreateReviewRequest;
+import com.mohaeng.backend.place.dto.request.UpdateReviewRequest;
 import com.mohaeng.backend.place.dto.response.FindAllReviewResponse;
 import com.mohaeng.backend.place.exception.PlaceNotFoundException;
 import com.mohaeng.backend.place.repository.PlaceRepository;
 import com.mohaeng.backend.place.repository.ReviewImageRepository;
 import com.mohaeng.backend.place.repository.ReviewRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,6 +35,10 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final MemberRepository memberRepository;
     private final ReviewImageRepository reviewImageRepository;
+    private final AmazonS3Service amazonS3Service;
+
+    private final EntityManager entityManager;
+
 
     public List<FindAllReviewResponse> getAllReview(Long id) {
         Place findPlace = placeRepository.findById(id)
@@ -39,11 +50,20 @@ public class ReviewService {
     }
 
     @Transactional
-    public void createReview(String email, Long placeId, CreateReviewRequest createReviewRequest, List<String> filaNameList) {
+    public void createReview(String email, Long placeId, CreateReviewRequest createReviewRequest, List<String> fileNameList) {
         Place findPlace = placeRepository.findById(placeId)
                 .orElseThrow(() -> new PlaceNotFoundException("NOT_EXIST_PLACE"));
         Member findMember = memberRepository.findByEmailAndDeletedDateIsNull(email)
                 .orElseThrow(() -> new MemberNotFoundException());
+
+        LocalDateTime createdDate = createReviewRequest.getCreatedDate();
+        if (createdDate == null) {
+            // Set a default value or handle the error
+            createdDate = LocalDateTime.now();
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formattedCreatedDate = createdDate.format(formatter);
         Review review = Review.builder()
                 .place(findPlace)
                 .member(findMember)
@@ -51,14 +71,18 @@ public class ReviewService {
                 .rating(createReviewRequest.getRating())
                 .content(createReviewRequest.getContent())
                 .reviewImageList(new ArrayList<>())
+                .createdDate(createdDate)
                 .build();
 
         reviewRepository.save(review);
         findMember.addReview(review);
         findPlace.addReview(review);
 
-        registerImage(filaNameList, review);
-
+        if (fileNameList != null && !fileNameList.isEmpty()) {
+            registerImage(fileNameList, review);
+        }
+        entityManager.flush();
+        entityManager.clear();
     }
 
     private void registerImage(List<String> filaNameList, Review review) {
@@ -73,5 +97,50 @@ public class ReviewService {
             review.addReviewImage(reviewImage);
             reviewImageRepository.save(reviewImage);
         }
+        entityManager.flush();
+        entityManager.clear();
+    }
+
+    @Transactional
+    public void updateReview(Long placeId, UpdateReviewRequest updateReviewRequest, List<String> fileNameList) {
+        Review review = reviewRepository.findById(placeId)
+                .orElseThrow(() -> new ReviewNotFoundException());
+        review.update(updateReviewRequest.getTitle(), updateReviewRequest.getContent(), updateReviewRequest.getRating());
+
+        // Delete existing images
+        for (ReviewImage reviewImage : review.getReviewImageList()) {
+            reviewImageRepository.delete(reviewImage);
+        }
+        review.getReviewImageList().clear();
+
+        // Add new images
+        // Add new images
+        if (fileNameList != null) {
+            registerImage(fileNameList, review);
+        }
+//        registerImage(fileNameList, review);
+        entityManager.flush();
+        entityManager.clear();
+    }
+
+    @Transactional
+    public void deleteReview(Long reviewId) {
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ReviewNotFoundException());
+
+
+        // Delete images from storage service
+        for (ReviewImage reviewImage : review.getReviewImageList()) {
+            amazonS3Service.deleteFile(reviewImage.getImageUrl() + "/" + reviewImage.getImageName());
+        }
+
+        // Delete related images
+        reviewImageRepository.deleteAll(review.getReviewImageList());
+
+        // Delete review and related images from database
+        reviewRepository.delete(review);
+        // Clear JPA cache
+        entityManager.flush();
+        entityManager.clear();
     }
 }
