5388c3e13c926afd885e7a55826debfbe9ff0288
binaryhong <binaryhong@gmail.com>
2023-04-08 03:07:34 +0900

    Feat: createDate 추가, rating double to String

--- backend/src/main/java/com/mohaeng/backend/place/dto/request/CreateReviewRequest.java
+++ backend/src/main/java/com/mohaeng/backend/place/dto/request/CreateReviewRequest.java
@@ -1,10 +1,16 @@
 package com.mohaeng.backend.place.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 public class CreateReviewRequest {
     private String title;
     private String content;
-    private double rating;
+    private String rating;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime createdDate;
 }
