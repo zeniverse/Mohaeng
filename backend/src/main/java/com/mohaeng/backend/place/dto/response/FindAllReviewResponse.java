1c9919b43b74496daafa24354597b43f808a095e
win9 <krrr883@gmail.com>
2023-04-07 16:54:50 +0900

    Feat: 리뷰 작성시 작성자의 프로필을 보도록 수정 [#108]

--- backend/src/main/java/com/mohaeng/backend/place/dto/response/FindAllReviewResponse.java
+++ backend/src/main/java/com/mohaeng/backend/place/dto/response/FindAllReviewResponse.java
@@ -1,24 +1,39 @@
 package com.mohaeng.backend.place.dto.response;
 
 import com.mohaeng.backend.place.domain.Review;
+import com.mohaeng.backend.place.domain.ReviewImage;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class FindAllReviewResponse {
     private String memberName;
+    private String memberImage;
     private double rating;
     private int likeCount;
     private String content;
-    private String imgUrl;
+    private List<String> imgUrl = new ArrayList<>();
 
     public static FindAllReviewResponse of(Review review) {
         return new FindAllReviewResponse(
                 review.getMember().getName(),
+                review.getMember().getImageURL() + "/" + review.getMember().getImageName(),
                 review.getRating(),
                 review.getLikeCount(),
                 review.getContent(),
-                review.getImgUrl());
+                getAllImageName(review));
+    }
+
+    private static List<String> getAllImageName(Review review) {
+        List<ReviewImage> list = review.getReviewImageList();
+        List<String> imgUrl = new ArrayList<>();
+        for (ReviewImage reviewImage : list) {
+            imgUrl.add(reviewImage.getImageUrl() + "/" + reviewImage.getImageName());
+        }
+        return imgUrl;
     }
 }
