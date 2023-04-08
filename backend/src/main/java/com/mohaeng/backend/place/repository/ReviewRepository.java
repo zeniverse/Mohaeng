fea52dbb65be2dab7cf01fae1c8677a78d2e024c
binaryhong <binaryhong@gmail.com>
2023-04-09 01:22:05 +0900

    Feat: getPlaceReview에서 Page로 출력하기 위한 findAllByPlaceID 추가

--- backend/src/main/java/com/mohaeng/backend/place/repository/ReviewRepository.java
+++ backend/src/main/java/com/mohaeng/backend/place/repository/ReviewRepository.java
@@ -1,7 +1,11 @@
 package com.mohaeng.backend.place.repository;
 
 import com.mohaeng.backend.place.domain.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findAllByPlaceId(Long id, Pageable pageable);
 }
