08b9825498b621465cd87ee636a48a717683d599
binaryhong <binaryhong@gmail.com>
2023-04-10 04:43:57 +0900

    Feat: API_KEY 분리를 위한 코드 변경

--- backend/src/main/java/com/mohaeng/backend/place/service/PlaceService.java
+++ backend/src/main/java/com/mohaeng/backend/place/service/PlaceService.java
@@ -16,6 +16,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.mock.web.MockMultipartFile;
@@ -55,15 +56,17 @@ public class PlaceService {
     @PersistenceContext
     private EntityManager entityManager;
 
-    //    private static final String API_KEY = "dpTiMvKcFS8NVB1nRTahfmZTMala0uVdt7qu81eNIxznRol2OcYVskpBXHGIfAEIQf1eY2b%2FjMA4uu5ztw8heg%3D%3D";
-//    private static final String API_KEY = "YDqng1hgvEKmXZVWZANhmv%2BKe1qPepQj%2BIxBkqBQTUyLG3XCiAXQtMzCMbNppL8OnNL8sPqvbxybFGIxrRpPnA%3D%3D"; // 승구님 키
-    private static final String API_KEY = "LEZGPi1UafewX40Vl5Yx8J4xwPliJNjaGSVMR8tOLVC7BTWBJMiQLb2gl12QNctUovP3VVtG6qPnrWteZGePOQ%3D%3D"; // 지혜님 키
-//    private static final String API_KEY = "DZSCfwbqP6kQHPDOAlDjWAhu63OBBX4BjGKHVNB2ocF6YW6Xpd6Do1IhFCg%2B1TfYiqZFngr57pUk3Tvs%2FYUukw%3D%3D"; // 지혜님 키
-
-    private static final String BASE_URL = "https://apis.data.go.kr/B551011/KorService1/areaBasedList1?serviceKey=" + API_KEY + "&pageNo=1&numOfRows=12100&MobileApp=AppTest&_type=xml&MobileOS=ETC&arrange=A&contentTypeId=12";
-    private static final String BASE_URL2 = "https://apis.data.go.kr/B551011/KorService1/detailCommon1?serviceKey=" + API_KEY + "&MobileOS=ETC&MobileApp=AppTest&_type=xml&contentId=&contentTypeId=12&&overviewYN=Y";
-
+    @Value("${API_KEY}")
+    private String API_KEY;
     private String fileUrl;
+
+    private String getBaseUrl() {
+        return "https://apis.data.go.kr/B551011/KorService1/areaBasedList1?serviceKey=" + API_KEY + "&pageNo=1&numOfRows=12100&MobileApp=AppTest&_type=xml&MobileOS=ETC&arrange=A&contentTypeId=12";
+    }
+
+    private String getBaseUrl2() {
+        return "https://apis.data.go.kr/B551011/KorService1/detailCommon1?serviceKey=" + API_KEY + "&MobileOS=ETC&MobileApp=AppTest&_type=xml&contentId=&contentTypeId=12&&overviewYN=Y";
+    }
 
     public void savePlace() throws IOException {
         // 1. 리소스 폴더에 있는 파일을 읽어들인다
@@ -81,7 +84,7 @@ public class PlaceService {
 
     //    @Scheduled(cron = "0 0 5 * * ?") #TODO
     public List<Place> getPlaces() throws IOException, ParserConfigurationException, SAXException {
-        Document doc = getXmlDocument(BASE_URL);
+        Document doc = getXmlDocument(getBaseUrl());
         List<Place> places = new ArrayList<>();
         NodeList nodeList = doc.getElementsByTagName("item");
         for (int i = 0; i < nodeList.getLength(); i++) {
@@ -118,7 +121,7 @@ public class PlaceService {
     }
 
     private String getOverview(String contentId) {
-        String urlStr = BASE_URL2.replace("contentId=", "contentId=" + contentId);
+        String urlStr = getBaseUrl2().replace("contentId=", "contentId=" + contentId);
         Document doc;
         NodeList nodeList;
         String overviewText = "";
