241821bf0a1295573bab0d70450a9dee8b95a83c
binaryhong <binaryhong@gmail.com>
2023-04-13 16:02:32 +0900

    Refactor: main페이지 url place/main으로 변경

--- backend/src/main/java/com/mohaeng/backend/place/controller/PlaceController.java
+++ backend/src/main/java/com/mohaeng/backend/place/controller/PlaceController.java
@@ -146,7 +146,7 @@ public class PlaceController {
         return ResponseEntity.ok().body(BaseResponse.success("OK", response));
     }
 
-    @GetMapping("/main")
+    @GetMapping("/place/main")
     public ResponseEntity getPlaceReviewsByRatingTop10(
             @RequestParam(defaultValue = "1") int page) {
         Page<Review> reviews = reviewService.getAllReviewsByRatingTop10(page);
