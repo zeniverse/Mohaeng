bf36ea855a3b1246754e6c7655eec0d87f7ec204
binaryhong <binaryhong@gmail.com>
2023-04-13 16:00:02 +0900

    Refactor: getPlace -> getPlaceByContentId 메서드명 변경

--- backend/src/main/java/com/mohaeng/backend/place/controller/PlaceController.java
+++ backend/src/main/java/com/mohaeng/backend/place/controller/PlaceController.java
@@ -63,7 +63,7 @@ public class PlaceController {
 
     @GetMapping("/place/{contentId}")
     public ResponseEntity<PlaceDTO> getPlace(@PathVariable String contentId) throws IOException, ParserConfigurationException, SAXException {
-        Place place = placeService.getPlace(contentId);
+        Place place = placeService.getPlaceByContentId(contentId);
         if (place == null) {
             return ResponseEntity.notFound().build();
         }
