43ea56452631863556de259bcc42d1de2bf54cc2
binaryhong <binaryhong@gmail.com>
2023-04-14 15:10:46 +0900

    Feat: content 댓글 내용이 아닌, Place Overview를 반영하도록 수정

--- backend/src/main/java/com/mohaeng/backend/place/dto/MainPageDto.java
+++ backend/src/main/java/com/mohaeng/backend/place/dto/MainPageDto.java
@@ -1,11 +1,13 @@
 package com.mohaeng.backend.place.dto;
 
 import com.mohaeng.backend.place.domain.Place;
-import com.mohaeng.backend.place.domain.Review;
 import com.mohaeng.backend.place.service.PlaceService;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
+import java.util.Map;
 
 @Getter
 @Builder
@@ -17,17 +19,19 @@ public class MainPageDto {
     private String averageRating;
     private String firstImage;
 
-    public static MainPageDto Of(Place place, Review review, PlaceService placeService){
+    public static MainPageDto Of(Place place, PlaceService placeService, Map<String, String> overviews){
         String firstImage = place.getFirstImage();
         if (firstImage == null || firstImage.isEmpty()) {
             firstImage = placeService.getFirstImage();
         }
         double averageRating = placeService.getAverageRatingForPlace(place.getId());
+        List<String> overviewList = placeService.getPlaceOverview(place.getContentId());
+        String overview = String.join(" ", overviewList);
 
         return MainPageDto.builder()
                 .placeId(place.getId())
                 .name(place.getName())
-                .content(review.getContent())
+                .content(overview)
                 .averageRating(String.valueOf(averageRating))
                 .firstImage(firstImage)
                 .build();
