6fa7d88b66bd56815fe01ac41f979bcae05b6ecc
Itz_me_Hazel <deaese94@gmail.com>
2023-04-18 15:58:04 +0900

    Refactor : 컨테이너 여백 수정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -21,81 +21,81 @@ const nextConfig = {
     return [
       // {
       //   source: "/:path*",
-      //   destination: "http://localhost:8080/:path*",
+      //   destination: "http://3.24.141.151:8080/:path*",
       // },
       // 코스 상세를 보려면 위 코드는 주석 처리하고 아래 코드는 주석을 풀어주세요.
       // 다른 url 경로 추가
       {
         source: "/api/course/:path*",
-        destination: "http://localhost:8080/api/course/:path*",
+        destination: "http://3.24.141.151:8080/api/course/:path*",
       },
       {
         source: "/api/myPage/course/bookMark",
-        destination: "http://localhost:8080/api/myPage/course/bookMark",
+        destination: "http://3.24.141.151:8080/api/myPage/course/bookMark",
       },
       {
         source: "/api/myPage/myReview",
-        destination: "http://localhost:8080/api/myPage/myReview",
+        destination: "http://3.24.141.151:8080/api/myPage/myReview",
       },
       {
         source: "/api/myPage/course",
-        destination: "http://localhost:8080/api/myPage/course",
+        destination: "http://3.24.141.151:8080/api/myPage/course",
       },
       {
         source: "/login/:path*",
-        destination: "http://localhost:8080/login/:path*",
+        destination: "http://3.24.141.151:8080/login/:path*",
       },
       {
         source: "/loginInfo/:path*",
-        destination: "http://localhost:8080/loginInfo/:path*",
+        destination: "http://3.24.141.151:8080/loginInfo/:path*",
       },
       {
         source: "/loginInfo",
-        destination: "http://localhost:8080/loginInfo",
+        destination: "http://3.24.141.151:8080/loginInfo",
       },
       {
         source: "/oauth/token/:path*",
-        destination: "http://localhost:8080/oauth/token/:path*",
+        destination: "http://3.24.141.151:8080/oauth/token/:path*",
       },
       {
         source: "/api/places/:path*",
-        destination: "http://localhost:8080/api/places/:path*",
+        destination: "http://3.24.141.151:8080/api/places/:path*",
       },
       {
         source: "/api/place/:path*",
-        destination: "http://localhost:8080/api/place/:path*",
+        destination: "http://3.24.141.151:8080/api/place/:path*",
       },
       {
         source: "/api/place/overview/:path*",
-        destination: "http://localhost:8080/api/place/overview/:path*",
+        destination: "http://3.24.141.151:8080/api/place/overview/:path*",
       },
       {
         source: "/api/review/:path*",
-        destination: "http://localhost:8080/api/review/:path*",
+        destination: "http://3.24.141.151:8080/api/review/:path*",
       },
       {
         source: "/api/review/detail/:path*",
-        destination: "http://localhost:8080/api/review/detail/:path*",
+        destination: "http://3.24.141.151:8080/api/review/detail/:path*",
       },
       {
         source: "/api/review/:placeId/date",
-        destination: "http://localhost:8080/api/review/:placeId/date",
+        destination: "http://3.24.141.151:8080/api/review/:placeId/date",
       },
       {
         source: "/api/review/:placeId/rating",
-        destination: "http://localhost:8080/api/review/:placeId/rating",
+        destination: "http://3.24.141.151:8080/api/review/:placeId/rating",
       },
       {
         source: "/api/myPage/place/bookMark",
-        destination: "http://localhost:8080/api/myPage/place/bookMark",
+        destination: "http://3.24.141.151:8080/api/myPage/place/bookMark",
       },
       {
         source: "/api/myPage/:path*",
-        destination: "http://localhost:8080/api/myPage/:path*",
+        destination: "http://3.24.141.151:8080/api/myPage/:path*",
       },
       {
         source: "/api/place/main/:path",
-        destination: "http://localhost:8080/api/place/main/:path",
+        destination: "http://3.24.141.151:8080/api/place/main/:path",
       },
     ];
   },
