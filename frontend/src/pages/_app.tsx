9c46db5bd4f6628ba2d3d0797a83e016de1cc74a
Itz_me_Hazel <deaese94@gmail.com>
2023-04-19 18:33:04 +0900

    Fix : config 수정

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -38,34 +38,3 @@ function MyApp({ Component, pageProps: { ...pageProps } }: AppProps) {
 }
 
 export default MyApp;
-
-// _app.tsx에서 전역으로 getInitialProps를 적용하게 되면, 모든 페이지가 서버 사이드 렌더링이 됨 (자동최적화 불가)
-// MyApp.getInitialProps = async (appContext: AppContext) => {
-//   const appProps = await App.getInitialProps(appContext);
-//   // 서버 사이드 쿠키 얻어오기
-//   console.log(ctx);
-//   try {
-//     const response = await axios.get(
-//       `http://localhost:8080/oauth/token/refresh`,
-//       {
-//         headers: {
-//           "Refresh-Token": `${refreshToken}`,
-//         },
-//         withCredentials: true,
-//       }
-//     );
-//     const accessToken = response.data;
-//     console.log(response);
-//     ctx.res?.setHeader("set-cookie", `accessToken=${accessToken}; path=/;`);
-//   } catch (err: any) {
-//     console.log(err);
-//   }
-//   const allCookies = cookies(ctx);
-//   const accessTokenByCookie = allCookies["accessToken"];
-//   if (accessTokenByCookie !== undefined) {
-//     const refreshTokenByCookie = allCookies["refreshToken"] || "";
-//     saveToken(accessTokenByCookie, refreshTokenByCookie);
-//   }
-
-//   return { ...appProps };
-// };
