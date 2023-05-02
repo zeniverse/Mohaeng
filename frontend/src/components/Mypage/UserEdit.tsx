05e1e8da807b2760cfbe56f3c37277281952687d
Itz_me_Hazel <deaese94@gmail.com>
2023-05-02 16:06:55 +0900

    Refactor : 유저 정보 수정 페이지 변경

--- frontend/src/components/Mypage/UserEdit.tsx
+++ frontend/src/components/Mypage/UserEdit.tsx
@@ -134,6 +134,7 @@ const UserEdit = () => {
                 <input
                   className={styles["Input"]}
                   type="text"
+                  value={editName}
                   placeholder={nickname}
                   onChange={(e) => setEditName(e.target.value)}
                 />
