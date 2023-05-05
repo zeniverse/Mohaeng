b59b33f22e09b6a06e3c6fff06444d5428ab1203
binaryhong <binaryhong@gmail.com>
2023-05-05 21:24:51 +0900

    Refactor: 쓰지 않는 import문 삭제

--- backend/src/main/java/com/mohaeng/backend/place/initializer/PlaceInitializer.java
+++ backend/src/main/java/com/mohaeng/backend/place/initializer/PlaceInitializer.java
@@ -13,7 +13,6 @@ import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
